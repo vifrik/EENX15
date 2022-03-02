344433433926e8d5b9a86ba5d0a15b3ed3e21e60
Viktor Frideen <frideen@student.chalmers.se>
2022-03-02 21:36:19 +0100

    Update ppc

--- aruco/cpp/ppc.cpp
+++ aruco/cpp/ppc.cpp
@@ -1,15 +1,21 @@
 #include <cmath>
+#include <iostream>
+#include "purePursuitController.h"
 
 
 int main(int argc, char **argv) {
+    std::vector<Coord> path;
+
+    // generate path
     for (int i = 0; i < 1000; i++) {
-        coords[i].x = i;
-        coords[i].y = 5;
+        Coord coord = Coord(i/100.0, sin((i/100.0)*500.0*M_PI/180.0));
+        path.push_back(coord);
     }
 
-    Coord pos = {15, 15};
+    purePursuitController ppc = purePursuitController(path, 1.0);
+    Coord target1 = ppc.getTarget(Coord(0,0));
+    Coord target2 = ppc.getTarget(Coord(target1.x,target1.y));
 
-    for (int i = 0; i < 1000; i++) {
-        double d = calculateDistance(pos, coords[i]);
-    }
+    std::cout << target1.x << " " << target1.y << std::endl;
+    std::cout << target2.x << " " << target2.y << std::endl;
 }
