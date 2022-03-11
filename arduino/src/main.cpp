0273abc470e91a40133ff896473d093ad20afb89
Viktor Frideen <frideen@student.chalmers.se>
2022-03-11 18:55:19 +0100

    Update test serial file

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,12 +1,9 @@
 #include <ArduinoSerialProtocol.h>
 
 // The payload that will be sent to the other device
-struct SamplePayload
+struct __attribute__((packed)) SamplePayload
 {
-    int8_t A;
     uint8_t B;
-    int16_t C;
-    uint16_t D;
 } payload;
 
 ArduinoSerialProtocol protocol(&Serial, (uint8_t *)&payload, sizeof(payload));
@@ -15,13 +12,7 @@ uint8_t receiveState;
 void setup()
 {
     Serial.begin(9600);
-
-    payload.A = 0;
     payload.B = 0;
-    payload.C = 0;
-    payload.D = 0;
-
-    Serial.write("Hello world!");
 }
 
 void loop()
@@ -30,13 +21,6 @@ void loop()
 
     if (receiveState == ProtocolState::SUCCESS)
     {
-        ++payload.A;
-        ++payload.B;
-        ++payload.C;
-        ++payload.D;
-
-        protocol.send();
+        // perform something, e.g. servo motor
     }
-
-    delay(1000);
 }
