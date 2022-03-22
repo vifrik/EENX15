ece1963eda012bba6d661d632efa2a774783ce55
Wilmer Eriksson <wilmere@student.chalmers.se>
2022-03-22 11:38:16 +0100

    add crc check

--- arduino/src/vera_dolly/vera_dolly.cpp
+++ arduino/src/vera_dolly/vera_dolly.cpp
@@ -27,7 +27,21 @@ struct PositionData
     float x;
     float y;
     float rz;
+    uint16_t crc;
 } positionData;
+
+uint16_t crc16(unsigned char* data, size_t len) {
+    uint16_t crc = 0;
+    for (size_t i = 0; i < len; i++) {
+        crc ^= data[i];
+        for (int k = 0; k < 8; k++) {
+            crc = crc & 1 ? (crc >> 1) ^ 0xa001 : crc >> 1;
+        }
+    }
+    if(crc < 256) crc |= 1 << 8;
+    if (!(crc & 255)) crc |= 1 << 0;
+    return crc;
+}
 
 ServoManager servoManager = ServoManager(SERVO_PIN);
 Motor motor = Motor(MOTOR_FIRST_DIR_PIN, MOTOR_SECOND_DIR_PIN, MOTOR_SPEED_PIN);
@@ -56,9 +70,13 @@ void setup() {
 }
 
 void loop() {
-    byte bytesBuffer[13]; // 4 bytes * 3 floats + null terminator, 4+4+4+1 = 13
-    int size = SerialUSB.readBytesUntil(byte(0), bytesBuffer, 13);
-    memcpy(&positionData, bytesBuffer, 12);
+    unsigned char bytesBuffer[15]; // 4 bytes * 3 floats + null terminator, 4+4+4+1 = 13
+    int size = SerialUSB.readBytesUntil(byte(0), bytesBuffer, 15);
+    if(size != 14) return;
+
+    memcpy(&positionData, bytesBuffer, 14);  
+    uint16_t c = crc16(bytesBuffer, 12);
+    if(c != positionData.crc) return;
 
     if (positionData.x < 0 || positionData.x > 10) 
         return;
