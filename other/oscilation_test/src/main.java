2c3a8f895cd2dc7185b2f7fb2eee0fcc80615311
Viktor Frideen <frideen@student.chalmers.se>
2022-03-22 20:21:40 +0100

    Remove PI regulator from oscilation test

--- other/oscilation_test/src/main.java
+++ other/oscilation_test/src/main.java
@@ -1,34 +1,12 @@
 import processing.core.PApplet;
 
 public class main extends PApplet{
-    class PIRegulator {
-        float kp;
-        float ki;
-        float prevError = 0;
-        float integral = 0;
-
-        public PIRegulator(float proportionalConst, float integratorConst) {
-            this.kp = proportionalConst;
-            this.ki = integratorConst;
-        }
-
-        public float update(float reference, float actual) {
-            float error = reference - actual;
-            float propTerm = kp * error;
-
-            integral += error * 1/100;
-            float intTerm = ki * integral;
-
-            return intTerm + propTerm;
-        }
-    }
     class Car {
         private static final int WIDTH = 20;
         private static final int HEIGHT = 12;
         float x, y, rz;
         float delta;
         float vel;
-        PIRegulator piRegulator = new PIRegulator(1,1);
 
         public Car(float x, float y, float rz, float vel) {
             this.x = x;
@@ -63,34 +41,11 @@ public class main extends PApplet{
     }
 
     Car car = new Car(0, 100, 45*PI/180, 2f);
-    PIRegulator piRegulator = new PIRegulator(0.1f, 2f);
-    float delta = 0;
-    public void draw() {
-        //piRegulator.integral = 0;
 
+    public void draw() {
         background(0);
         stroke(255,0,0);
         line(0,100,1200,100);
-        /*strokeWeight(3);
-        stroke(255,0,0);
-        line(0, 200, 200, 200);
-        line(200, 200, 200, 100);
-        line(200, 100, 1200, 100);
-
-        stroke(0,0,255);
-        delta = 0;
-        for (int i = 0; i < 200; i++) {
-            float diff = piRegulator.update(0, delta);
-            delta += diff;
-            System.out.println(i + " " + diff);
-            circle(i, 200 - delta*100, 2);
-        }
-
-        for (int i = 200; i < 1000; i++) {
-            float diff = piRegulator.update(1, delta);
-            delta += diff;
-            circle(i, 200 - delta*100, 2);
-        }*/
 
         float angle = atan2(100 - car.y, 1200 - car.x);
         float error = angle - car.rz;
