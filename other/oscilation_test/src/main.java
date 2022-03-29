7a3f8c91c91f1d71242ceeb0ae34cb654a02c134
Viktor Frideen <frideen@student.chalmers.se>
2022-03-29 12:13:12 +0200

    Add PPC to vis

--- other/oscilation_test/src/main.java
+++ other/oscilation_test/src/main.java
@@ -1,7 +1,74 @@
 import processing.core.PApplet;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main extends PApplet{
+    class Coord {
+        float x, y;
+
+        public Coord(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class PPC {
+            List<Integer> targetCandidates = new ArrayList<>();
+            public List<Coord> path = new ArrayList<>();
+
+            int index = 0;
+            boolean fromNegative = false;
+            private static final int MAX_STEPS = 100;
+
+            static double calculateDistance(Coord c1, Coord c2) {
+                return sqrt(pow(c1.x - c2.x, 2) + pow(c1.y - c2.y, 2));
+            }
+
+            public PPC(int pixelWidth) {
+                final float points = 1000;
+                float resolution = pixelWidth / points;
+                for (int i = 0; i < 1000; i++) {
+                    Coord position = new Coord(resolution * i, 50.0f * sin(i / 600.0f * 2 * PI) + 300.0f);
+                    path.add(position);
+                }
+
+            }
+
+            Coord getTarget(double lookahead, Coord position) {
+                for (int i = index; i < path.size(); i++) {
+                    Coord current = path.get(i);
+                    double distance = calculateDistance(position, current);
+
+                    if (fromNegative && distance - lookahead >= 0) {
+                        fromNegative = false;
+                        targetCandidates.add(i);
+                    } else if (distance - lookahead < 0) {
+                        fromNegative = true;
+                    }
+
+                    // To save time, break after 100 iterations if we found a candidate
+                    if (i > index + MAX_STEPS && !targetCandidates.isEmpty()) {
+                        break;
+                    }
+
+                    if (i == path.size() - 1 && targetCandidates.isEmpty()) {
+                        return path.get(path.size() - 1);
+                    }
+                }
+
+                if (targetCandidates.isEmpty()) {
+                    return path.get(index);
+                }
+
+                index = targetCandidates.get(targetCandidates.size() - 1);
+                return path.get(index);
+            }
+
+            boolean atEnd() {
+                return false;
+            }
+        }
     class Car {
         private static final int WIDTH = 20;
         private static final int HEIGHT = 12;
@@ -18,6 +85,9 @@ public class main extends PApplet{
 
         float angle = 0;
         float error = 0;
+
+        PPC ppc = new PPC(1200);
+        Coord target = new Coord(0,0);
 
         public Car(float x, float y, float rz, float vel) {
             this.x = x;
@@ -37,13 +107,15 @@ public class main extends PApplet{
             float d_rz = tan(delta) * vel / WIDTH;;
             this.rz += d_rz;
 
+            target = ppc.getTarget(100.0f, new Coord(xTrailer, yTrailer));
+
             this.xTrailer += vel * cos(rz - theta) * cos(theta);
             this.yTrailer += vel * cos(rz - theta) * sin(theta);
 
             float d_theta = vel * sin(rz - theta) / TRAILER_WIDTH;
             this.theta += d_theta;
 
-            angle = atan2(300 - yTrailer, 1200 - xTrailer);
+            angle = atan2(target.y - yTrailer, target.x - xTrailer);
             error = angle - theta + PI;
 
             float angleDesired = -(-angle-error);
@@ -51,7 +123,6 @@ public class main extends PApplet{
 
             oldAngleDesired = angleDesired;
 
-            float deltaTarget = -atan2((d_theta - d_angleDesired) * WIDTH, vel);
             float deltaTarget = -atan2((d_theta - d_angleDesired - d_rz) * WIDTH, vel);
 
             delta += max(-0.01f, min(0.01f, deltaTarget - delta));
@@ -90,7 +161,12 @@ public class main extends PApplet{
                 rotate(theta + PI);
 
                 rect(0,-HEIGHT / 2, -TRAILER_WIDTH, HEIGHT);
-                circle(0,0, 5);
+                pushStyle();
+                noFill();
+                stroke(0,0,255);
+                circle(0,0, 200);
+                popStyle();
+
                 pushStyle();
                     stroke(255,0,0);
                     line(0, 0, 100, 0);
@@ -105,20 +181,30 @@ public class main extends PApplet{
                 rect(0,-HEIGHT / 2, -TRAILER_WIDTH, HEIGHT);
                 circle(0,0, 5);
             popMatrix();
+
+            for (Coord pos : ppc.path) {
+                circle(pos.x, pos.y, 2);
+            }
+
+            pushStyle();
+            fill(255,0,0);
+            circle(target.x, target.y, 10);
+            popStyle();
         }
     }
 
+    Car car = new Car(0, 300, 15*PI/180+PI, -2f);
     public void settings(){
         size(1200, 800);
     }
 
-    Car car = new Car(300, 550, -15*PI/180+PI, -2f);
     public void draw() {
         background(0);
         stroke(255,0,0);
         line(0,300,1200,300);
 
         noStroke();
+
         if (keyCode == ENTER) {
             car.update();
         }
