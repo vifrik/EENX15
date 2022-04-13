857dd1b03e86e124ad091c263d384fd5497b5dc1
Viktor Frideen <frideen@student.chalmers.se>
2022-04-13 08:55:20 +0200

    Update figure template

--- other/figures/src/template.java
+++ other/figures/src/template.java
@@ -4,6 +4,27 @@ import processing.svg.*;
 
 public class template extends PApplet {
 
+    public void dottedLine(int x1, int y1, int x2, int y2, int spacing) {
+        float len = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        float steps = len / spacing;
+        for (int i = 0; i <= steps; i++) {
+            float x = lerp(x1, x2, i/steps);
+            float y = lerp(y1, y2, i/steps);
+            point(x, y);
+        }
+    }
+
+    public void arrow(int x1, int y1, int x2, int y2) {
+        line(x1, y1, x2, y2);
+        float angle = atan2(y2 - y1, x2 - x1);
+        pushMatrix();
+        translate(x2, y2);
+        rotate(angle);
+        line(-10, -10, 0, 0);
+        line(-10, 10, 0, 0);
+        popMatrix();
+    }
+
     public void settings(){
         size(800, 800);
         this.g = new PGraphics();
