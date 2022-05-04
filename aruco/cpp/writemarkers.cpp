40a035e411e895f575bf01f9149430fb18bf447d
Viktor Frideen <frideen@student.chalmers.se>
2022-05-04 11:50:11 +0200

    Add writemarkers file

--- aruco/cpp/writemarkers.cpp
+++ aruco/cpp/writemarkers.cpp
@@ -8,14 +8,13 @@ using namespace cv;
 
 int main(int argc, char **argv) {
     std::vector<Affine3d> markers;
-
-    Vec3d r1(-M_PI_2, 0, -M_PI_4);
-    Vec3d t1(1.0, 0, 0);
+    Vec3d r1(-M_PI_2, 0, M_PI_4);
+    Vec3d t1(1 - (0.115/2), 0, 0);
     Affine3d marker1(eulerRotation::eulerAnglesToRotationMatrix(r1), t1);
     markers.push_back(marker1);
 
-    Vec3d r2(0, 0, -M_PI_2);
-    Vec3d t2(0, 1.0, 0);
+    Vec3d r2(-M_PI_2, 0, -M_PI_4);
+    Vec3d t2(1 + (0.115/2), 0, 0);
     Affine3d marker2(eulerRotation::eulerAnglesToRotationMatrix(r2), t2);
     markers.push_back(marker2);
 
